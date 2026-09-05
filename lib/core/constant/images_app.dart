5a30c83674253b97055740ae2e8d770c434af7ad
Elshaarawy Hassan <elshaarawyhassan7@gmail.com>
2026-09-05 12:37:32 +0300

    add class imgases app

--- lib/core/constant/images_app.dart
+++ lib/core/constant/images_app.dart
@@ -1,6 +1,5 @@
 class ImagesApp{
- static final String LogoApp = 'assets/logo.png';
- static final String OnBording  = 'assets/screen.png'; 
- static final String CartHome ='assets/cart.jpg' ; 
+ static final String LogoApp = 'assets/images/logo_app.png';
+ 
   
 }
