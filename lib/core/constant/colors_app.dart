257ea33bf413a07a5b4329cafbfb405505f4fb03
Elshaarawy Hassan <elshaarawyhassan7@gmail.com>
2026-09-05 12:36:53 +0300

    add class App colors

--- lib/core/constant/colors_app.dart
+++ lib/core/constant/colors_app.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-class ColorsApp {
-  static final Color primColor = const Color(0xff2ECC71);
-  static final Color secColor = const Color(0xff121212);
-  static final Color thirdColor = const Color(0xffFFFFFF);
-  static final Color neutralColor = const Color(0xff1E1E1E);
+class AppClors {
+  static final Color primColor = const Color(0xff0D7C66);
+  static final Color secColor = const Color(0xff1A3C5E);
+  static final Color thirdColor = const Color(0xffF5A623);
+  static final Color neutralColor = const Color(0xff1C1C1E);
 }
